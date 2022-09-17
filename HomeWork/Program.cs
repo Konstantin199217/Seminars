9a222c5d576e495c15349873df565e0b46bd7326
Konstantin <kot199217@mail.ru>
2022-09-17 17:22:54 +0900

    homework01

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.WriteLine("Введите число а");
+ int a = int.Parse(Console.ReadLine());
+ Console.WriteLine("Введите число b");
+ int b = int.Parse(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("Число а больше числа b");
+}
+else if(a < b)
+{
+    Console.WriteLine(" Число b  больше а");
+}else
+{
+    Console.WriteLine(" а = b");
+}
