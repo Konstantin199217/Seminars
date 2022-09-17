268cbaa62c939bb1286ca1270d908a7fed86f67d
Konstantin <kot199217@mail.ru>
2022-09-17 18:46:14 +0900

    end homwork2

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите чило b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (max > c)
+{
+    Console.WriteLine("Большее число" + max);
+}
+else
+{
+    Console.WriteLine("Большее число" + c);
+}
