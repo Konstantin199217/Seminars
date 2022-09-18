1d595638a0000f71eeb18bd0d8bdbae6342dcb10
Konstantin <kot199217@mail.ru>
2022-09-18 16:56:05 +0900

    end HomeWork4

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,7 +1,12 @@
 ﻿Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
-int a = 0;
-while(number > a)
+int[] array = new int[number];
+for (int i = 0; i <= array.Length; i++)
 {
-    
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+    }
 }
+//Цикл for в C# предоставляет механизм итерации, в котором определенное условие проверяется перед выполнением каждой итерации.
+// Если число с "-" ? как делать?
