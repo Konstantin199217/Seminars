d0c4f345417db310e3c4cb00836e5f85d1af5de3
Konstantin <kot199217@mail.ru>
2022-09-28 23:41:29 +0900

    end Task30

--- SemTask30/Program.cs
+++ SemTask30/Program.cs
@@ -8,3 +8,27 @@ int ReadData(string msg)
     return number;
 }
 
+int[] Fill1darray(int len, int lowBord, int highBord)//Заполненый массив
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new System.Random().Next(lowBord, highBord + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int arrLen = ReadData("Введите длину массива ");
+int[] arr = Fill1darray(arrLen, 0, 1);
+PrintArray(arr);
+
