7eac31ed99e897be0dab40506b24932cff25f29e
Konstantin <kot199217@mail.ru>
2022-09-28 22:59:10 +0900

    added semtask30

---
+++ SemTask30/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке. 
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
