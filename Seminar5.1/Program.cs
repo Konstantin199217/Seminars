3365253fd9eac71e7d5ed1421f6ecb7d5b6f627b
Konstantin <kot199217@mail.ru>
2022-10-05 10:54:13 +0900

    End Seminar5.1

--- Seminar5.1/Program.cs
+++ Seminar5.1/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿//Задайте одномерный массив из 123 случайных чисел. 
+//Найдите количество элементов массива, 
+//значения которых лежат в отрезке [10,99].
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 99);
+    }
+    return array;
+}
+
+int[] Array = CreateArray(123);
+Console.WriteLine(String.Join(",", Array));
+Console.WriteLine();
+int[] ArrayRange = Array[10..99];
+int totalElements = ArrayRange.Count();
+foreach (var item in ArrayRange)
+{
+    Console.WriteLine(String.Join(",", item));
+}
+Console.WriteLine();
+Console.WriteLine(totalElements);
+
